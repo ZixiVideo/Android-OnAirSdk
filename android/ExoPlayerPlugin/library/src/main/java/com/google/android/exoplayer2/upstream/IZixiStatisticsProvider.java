d185a12aa6c4ed1defce5055f7d67063dfe7fc80
ZixiVideo <zixi.llc.il@gmail.com>
2017-02-26 13:09:53 +0200

    wrong repository

--- android/ExoPlayerPlugin/library/src/main/java/com/google/android/exoplayer2/upstream/IZixiStatisticsProvider.java
+++
@@ -1,8 +0,0 @@
-package com.google.android.exoplayer2.upstream;
-
-/**
- * Created by roy on 23-Feb-16.
- */
-public interface IZixiStatisticsProvider {
-    public int getStatistics(ZixiStatistics zixiStatistics);
-}
